981c7f7260f3fefb8b944ee9261429641569a1bc
kurokishi <kuroreidho@gmail.com>
2026-01-20 12:48:15 +0700

    Update __init__.py

--- app/ui/__init__.py
+++ app/ui/__init__.py
@@ -1,17 +1,19 @@
-# app/ui/__init__.py
+"""
+Package UI - komponen Streamlit untuk aplikasi Jadwal Dokter
+"""
 
-from .sidebar import render_sidebar
-from .tab_upload import render_upload_tab
-from .tab_analyzer import render_analyzer_tab
-from .tab_visualization import render_visualization_tab
-from .tab_settings import render_settings_tab
-from .tab_kanban_drag import render_drag_kanban
+from .home import render as render_home
+from .tab_upload import render as render_upload
+from .tab_schedule import render as render_schedule
+from .tab_kanban_drag import render as render_kanban
+from .tab_preferences import render as render_preferences
+from .tab_about import render as render_about
 
 __all__ = [
-    "render_sidebar",
-    "render_upload_tab",
-    "render_analyzer_tab",
-    "render_visualization_tab",
-    "render_settings_tab",
-    "render_drag_kanban"
+    'render_home',
+    'render_upload', 
+    'render_schedule',
+    'render_kanban',
+    'render_preferences',
+    'render_about'
 ]
