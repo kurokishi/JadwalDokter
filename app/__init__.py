d522a28d1b3d7aed3473b33fae1aaa0216edd505
kurokishi <kuroreidho@gmail.com>
2026-01-20 15:10:37 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,52 +1,10 @@
 """
-Package utama aplikasi Jadwal Dokter
+Jadwal Dokter App Package
 """
 
 __version__ = "1.0.0"
-__author__ = "Tim Pengembang"
+__author__ = "Jadwal Dokter Team"
 
-# Ekspor modul utama
-from . import config
-from . import main
-from . import utils
-
-# Ekspor dari core
-from .core import (
-    DataCleaner,
-    ScheduleParser,
-    TimeParser,
-    DataValidator,
-    TemplateParser
-)
-
-# Ekspor dari ui
-from .ui import (
-    render_home,
-    render_upload,
-    render_schedule,
-    render_kanban,
-    render_preferences,
-    render_about
-)
-
-__all__ = [
-    # Module
-    'config',
-    'main',
-    'utils',
-    
-    # Core classes
-    'DataCleaner',
-    'ScheduleParser',
-    'TimeParser',
-    'DataValidator',
-    'TemplateParser',
-    
-    # UI functions
-    'render_home',
-    'render_upload',
-    'render_schedule',
-    'render_kanban',
-    'render_preferences',
-    'render_about'
-]
+from app.config import AppConfig
+from app.utils.parser import JadwalHafisParser
+from app.core.data_manager import DataManager
