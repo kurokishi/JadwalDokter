01e624fdcf3d7b43c24046b23d414be63246e661
kurokishi <kuroreidho@gmail.com>
2026-01-20 15:17:45 +0700

    Update __init__.py

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,17 +1,6 @@
 """
-Package core - logika bisnis aplikasi Jadwal Dokter
+Core business logic module
 """
+from app.core.data_manager import DataManager
 
-from .cleaner import DataCleaner
-from .parser import ScheduleParser
-from .time_parser import TimeParser
-from .validator import DataValidator
-from .template_parser import TemplateParser
-
-__all__ = [
-    'DataCleaner',
-    'ScheduleParser', 
-    'TimeParser',
-    'DataValidator',
-    'TemplateParser'
-]
+__all__ = ['DataManager']
