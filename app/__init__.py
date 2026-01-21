3367944c793b252048a3200289927e9b0dd8d2b5
kurokishi <kuroreidho@gmail.com>
2026-01-21 08:06:44 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,3 @@ Jadwal Dokter App Package
 
 __version__ = "1.0.0"
 __author__ = "Jadwal Dokter Team"
-
-from app.config import AppConfig
-from app.utils.parser import JadwalHafisParser
-from app.core.data_manager import DataManager
