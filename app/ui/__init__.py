63ab682b4ad22d186f9856e56cf6b73061487f68
kurokishi <kuroreidho@gmail.com>
2026-01-21 08:03:28 +0700

    Update __init__.py

--- app/ui/__init__.py
+++ app/ui/__init__.py
@@ -1,18 +1,6 @@
 """
-UI components module
+Jadwal Dokter App Package
 """
-from app.ui.home import display_home_tab
-from app.ui.tab_upload import display_upload_tab
-from app.ui.tab_schedule import display_schedule_tab
-from app.ui.tab_kanban_drag import display_kanban_tab
-from app.ui.tab_preferences import display_preferences_tab
-from app.ui.tab_about import display_about_tab
 
-__all__ = [
-    'display_home_tab',
-    'display_upload_tab',
-    'display_schedule_tab',
-    'display_kanban_tab',
-    'display_preferences_tab',
-    'display_about_tab'
-]
+__version__ = "1.0.0"
+__author__ = "Jadwal Dokter Team"
