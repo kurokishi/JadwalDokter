5acfb75bd377ca29d9b1632ab1e5ed35cc27e870
kurokishi <kuroreidho@gmail.com>
2026-01-21 10:39:19 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 """
-Jadwal Dokter App Package
+Jadwal Dokter Converter Package
 """
-
-__version__ = "1.0.0"
+__version__ = "2.0.0"
 __author__ = "Jadwal Dokter Team"
+
+from .config import AppConfig
+from .utils import init_session_state
+
+# Export main components
+__all__ = ['AppConfig', 'init_session_state', '__version__']
