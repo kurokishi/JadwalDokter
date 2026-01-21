f8dc85fd3ffaee16f1ba2e6243bf26d12bb14ed3
kurokishi <kuroreidho@gmail.com>
2026-01-21 08:21:07 +0700

    Update __init__.py

--- app/ui/__init__.py
+++ app/ui/__init__.py
@@ -1,16 +1,5 @@
 """
-Utilities module for Jadwal Dokter App
+UI components module
 """
-from app.utils.parser import JadwalHafisParser
-from app.utils.cleaner import DataCleaner
-from app.utils.time_parser import TimeParser
-from app.utils.validator import DataValidator
-from app.utils.template_parser import TemplateParser
 
-__all__ = [
-    'JadwalHafisParser',
-    'DataCleaner',
-    'TimeParser', 
-    'DataValidator',
-    'TemplateParser'
-]
+# Empty file - just to make it a package
